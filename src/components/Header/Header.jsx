1cce7238d87aadf75a7861555811c5791be2eb1a
MishynaIra <gortenzia1987@gmail.com>
2023-08-07 14:32:16 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Search from "./Search/Search";
+
+const Header = () => {
+    return (
+      <div>
+          <h2>Event Planner</h2>
+
+            <Search/>
+      </div>
+    )
+}
+
+export default Header;
